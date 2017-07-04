0b1f0f94ab8710bc1cd5eee0ca5c2e13857c303e
Sebastian P <Sebastian P>
2017-07-04 19:26:28 +0200

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Sebastian on 04.07.2017.
+ */
+import com.github.seeb73.JakZostacPrograsmista.Przelew;
+
+public class Test {
+    public static void main(String[] args) {
+        double amount = 199;
+        String title = "Przelew do ZUSu";
+        String nazwaOdbiorcy = "Zakład ubezpieczeń społecznych";
+        String rachunekOdbiorcy ="78 1010 1023 0000 2613 9520 0000"; // zdrowotne ;)
+
+        Przelew przelew = new Przelew(title, amount, nazwaOdbiorcy, rachunekOdbiorcy); // dodaj więcej pól do konstruktora
+        przelew.wypiszDanePrzelewu();
+    }
+}
---
+++ src/com/github/seeb73/JakZostacPrograsmista/Przelew.java
@@ -0,0 +1,32 @@
+package com.github.seeb73.JakZostacPrograsmista;
+
+/**
+ * Created by Sebastian on 04.07.2017.
+ */
+
+public class Przelew {
+
+    // definicje typów danych
+    public  double amount;
+    public  String title;
+    public  String nazwaOdbiorcy;
+    public  String rachunekOdbiorcy;
+    private String rachunekNadawcy = "78 1240 0001 3684 0110 7766 8444";
+    private String walutaPrzelewu = "PLN";
+
+    public Przelew(String title, double amount,String nazwaOdbiorcy, String rachunekOdbiorcy) {
+        this.rachunekNadawcy = rachunekNadawcy;
+        this.title = title;
+        this.amount = amount;
+        this.nazwaOdbiorcy = nazwaOdbiorcy;
+        this.rachunekOdbiorcy = rachunekOdbiorcy;
+    }
+
+    public void wypiszDanePrzelewu()
+    {
+        System.out.println("Odbiorca: " + this.nazwaOdbiorcy );
+        System.out.println("Tytułem: " + this.title +" Kwota: " + this.amount + " " + walutaPrzelewu );
+        System.out.println("Z rachunku: " + this.rachunekNadawcy + ", Na rachunek: " + this.rachunekOdbiorcy );
+    }
+
+}
