ae89294cc6f1dd8e2d9ac57f11db42c4dff4aacd
Seeb73 <seeb@seeb.net.pl>
2017-07-04 22:57:31 +0200

    Nowy plik - treść ćwiczenia.

--- src/com/github/seeb73/JakZostacPrograsmista/Przelew.java
+++ src/com/github/seeb73/JakZostacPrograsmista/Przelew.java
@@ -7,16 +7,16 @@ package com.github.seeb73.JakZostacPrograsmista;
 public class Przelew {
 
     // definicje typów danych
-    private double amount;
-    private String title;
+    private double kwotaPrzelewu;
+    private String przelewTytulem;
     private String nazwaOdbiorcy;
     private String rachunekOdbiorcy;
     private String rachunekNadawcy = "78 1240 0001 3684 0110 7766 8444";
     private String walutaPrzelewu = "PLN";
 
     public Przelew(String tytulem, double kwota,String odbiorca, String odbiorcaRachunek) {
-        title = tytulem;
-        amount = kwota;
+        przelewTytulem = tytulem;
+        kwotaPrzelewu = kwota;
         nazwaOdbiorcy = odbiorca;
         rachunekOdbiorcy = odbiorcaRachunek;
     }
@@ -24,7 +24,7 @@ public class Przelew {
     public void wypiszDanePrzelewu()
     {
         System.out.println("Odbiorca: " + nazwaOdbiorcy );
-        System.out.println("Tytułem: " + title +" Kwota: " + amount + " " + walutaPrzelewu );
+        System.out.println("Tytułem: " + przelewTytulem +" Kwota: " + kwotaPrzelewu + " " + walutaPrzelewu );
         System.out.println("Z rachunku: " + rachunekNadawcy + ", Na rachunek: " + rachunekOdbiorcy );
     }
 
