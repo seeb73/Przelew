ff2ea48166a3634c737410dab16104a82129e26c
Seeb73 <seeb@seeb.net.pl>
2017-07-04 21:26:57 +0200

    Drobne zmiany.

--- src/com/github/seeb73/JakZostacPrograsmista/Przelew.java
+++ src/com/github/seeb73/JakZostacPrograsmista/Przelew.java
@@ -7,15 +7,14 @@ package com.github.seeb73.JakZostacPrograsmista;
 public class Przelew {
 
     // definicje typów danych
-    public  double amount;
-    public  String title;
-    public  String nazwaOdbiorcy;
-    public  String rachunekOdbiorcy;
+    private double amount;
+    private String title;
+    private String nazwaOdbiorcy;
+    private String rachunekOdbiorcy;
     private String rachunekNadawcy = "78 1240 0001 3684 0110 7766 8444";
     private String walutaPrzelewu = "PLN";
 
     public Przelew(String title, double amount,String nazwaOdbiorcy, String rachunekOdbiorcy) {
-        this.rachunekNadawcy = rachunekNadawcy;
         this.title = title;
         this.amount = amount;
         this.nazwaOdbiorcy = nazwaOdbiorcy;
@@ -24,9 +23,9 @@ public class Przelew {
 
     public void wypiszDanePrzelewu()
     {
-        System.out.println("Odbiorca: " + this.nazwaOdbiorcy );
-        System.out.println("Tytułem: " + this.title +" Kwota: " + this.amount + " " + walutaPrzelewu );
-        System.out.println("Z rachunku: " + rachunekNadawcy + ", Na rachunek: " + this.rachunekOdbiorcy );
+        System.out.println("Odbiorca: " + nazwaOdbiorcy );
+        System.out.println("Tytułem: " + title +" Kwota: " + amount + " " + walutaPrzelewu );
+        System.out.println("Z rachunku: " + rachunekNadawcy + ", Na rachunek: " + rachunekOdbiorcy );
     }
 
 }
